6b2e5657554c5883b213dd2cc4cad06c40a4d009
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-04-10 18:48:29 +0300

    refactor: The fields for adding the Worker entity and the list_display of the User entity have been changed

--- aservice/admin.py
+++ aservice/admin.py
@@ -26,7 +26,40 @@ class ReviewsAdmin(admin.ModelAdmin):
 
 @admin.register(Worker)
 class WorkerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'position', 'specialization', 'user')
+    list_display = ('id', 'position', 'specialization', 'get_last_name', 'get_first_name',
+                    'get_email', 'get_phone_number')
+
+    def get_last_name(self, obj):
+        return obj.user.last_name
+    get_last_name.short_description = 'Фамилия'
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+    get_first_name.short_description = 'Имя'
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'E-mail'
+
+    def get_phone_number(self, obj):
+        return obj.user.phone_number
+    get_phone_number.short_description = 'Номер телефона'
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "user":
+            kwargs["queryset"] = User.objects.filter(role="ROLE_WORKER")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(role='ROLE_WORKER')
+
+
 
 
 
