708152f918c924771653cf24dd7cddfbaf0d3dd6
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-05-03 19:46:13 +0300

    refactor(Chat):Updated data serialization

--- chat/serializers.py
+++ chat/serializers.py
@@ -4,16 +4,26 @@ from chat.models import Conversation, Message
 
 
 class ConversationSerializer(serializers.ModelSerializer):
-    receiver = serializers.CharField(source='receiver.last_name', read_only=True)
+    receiver = serializers.SerializerMethodField()
+    sender = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
-        fields = ['id', 'receiver', 'created', 'accepted']
+        fields = ['id', 'receiver', 'created', 'accepted', 'sender']
+
+    def get_receiver(self, obj):
+        return obj.receiver.last_name, obj.receiver.first_name, obj.receiver.id
+
+    def get_sender(self, obj):
+        return obj.sender.last_name, obj.sender.first_name, obj.sender.id
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='user.last_name', read_only=True)
+    user = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
         fields = ['id', 'user', 'created', 'text', 'conversation']
+
+    def get_user(self, obj):
+        return obj.user.first_name, obj.user.last_name, obj.user.id
