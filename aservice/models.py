fe7819147063aebe96182a2d5ee3ec103b356629
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-05-05 22:15:48 +0300

    refactor(aservice):Fields of some entities have been updated and changed

--- aservice/models.py
+++ aservice/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator, MinLengthValidator
 from django.db import models
 from rest_framework.exceptions import ValidationError
 
@@ -15,10 +15,7 @@ phone_number_validator = RegexValidator(
 class User(AbstractUser):
     username = None
     email = models.EmailField(verbose_name='E-mail', max_length=255, unique=True)
-    phone_number = models.CharField(max_length=12, unique=True,
-                                    validators=[phone_number_validator],
-                                    verbose_name='Номер телефона')
-    role = models.CharField(max_length=100, verbose_name='РОЛЬ')
+    role = models.CharField(max_length=32, verbose_name='РОЛЬ')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -28,12 +25,13 @@ class User(AbstractUser):
 
 
 class Car(models.Model):
-    brand = models.CharField(max_length=100, verbose_name='Марка')
-    model = models.CharField(max_length=100, verbose_name='Модель')
-    year = models.PositiveIntegerField(null=True, verbose_name='Год')
-    color = models.CharField(max_length=50, blank=True, null=True, verbose_name='Цвет')
-    vin = models.CharField(max_length=17, unique=True, blank=False, null=False, verbose_name='VIN')
-    license_plate = models.CharField(max_length=15, unique=True, blank=False, null=False, verbose_name='Номер')
+    brand = models.CharField(max_length=128, validators=[MinLengthValidator(2)], verbose_name='Марка')
+    model = models.CharField(max_length=128, validators=[MinLengthValidator(2)], verbose_name='Модель')
+    year = models.CharField(max_length=4, validators=[MinLengthValidator(4)], null=True, blank=True, verbose_name='Год')
+    color = models.CharField(max_length=64, blank=True, null=True, verbose_name='Цвет')
+    vin = models.CharField(max_length=17, validators=[MinLengthValidator(17)], unique=True, blank=False, null=False,
+                           verbose_name='VIN')
+    license_plate = models.CharField(max_length=15, unique=True, blank=True, null=True, verbose_name='Гос. номер')
     photo = models.ImageField(upload_to='car_photos/%Y/%m/%d', blank=True, null=True, verbose_name='Фото машины')
     user = models.ForeignKey('User', blank=True, null=True, on_delete=models.CASCADE, related_name='users_car',
                              verbose_name='Владелец')
@@ -43,7 +41,7 @@ class Car(models.Model):
 
 
 class Service(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Название')
+    title = models.CharField(max_length=128, validators=[MinLengthValidator(5)], verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     price = models.PositiveIntegerField(blank=True, null=True, verbose_name='Стоимость')
     worker = models.ManyToManyField('Worker', blank=False, related_name='workers_sr', verbose_name='Рабочий')
@@ -59,6 +57,7 @@ class Worker(models.Model):
                                        verbose_name='Специальность')
     experience = models.PositiveIntegerField(verbose_name='Стаж')
     user = models.OneToOneField('User', on_delete=models.CASCADE, related_name='users_wr', verbose_name='Механик')
+    photo = models.ImageField(upload_to='workers_photos/', blank=True, null=True, verbose_name='Фото механика')
 
     def __str__(self):
         return f"{self.user.pk}"
@@ -66,21 +65,30 @@ class Worker(models.Model):
 
 class Appointment(models.Model):
     class Status(models.TextChoices):
-        CONFIRMED = 'ПОДТВЕРЖДЕНА'
-        IN_PROGRESS = 'В ПРОЦЕССЕ'
-        COMPLETED = 'ЗАВЕРШЕНА'
-        CANCELLED = 'ОТМЕНЕНА'
+        CONFIRMED = 'CONFIRMED', 'ПОДТВЕРЖДЕНА'
+        IN_PROGRESS = 'IN_PROGRESS', 'В ПРОЦЕССЕ'
+        COMPLETED = 'COMPLETED', 'ЗАВЕРШЕНА'
+        CANCELLED = 'CANCELLED', 'ОТМЕНЕНА'
 
     date = models.DateField(verbose_name='День')
     time = models.TimeField(verbose_name='Время')
-    status = models.CharField(max_length=20, choices=Status.choices, default=Status.CONFIRMED, verbose_name='Статус')
+    status = models.CharField(choices=Status.choices, default=Status.CONFIRMED, verbose_name='Статус')
     total_price = models.PositiveIntegerField(default=0, blank=True, null=True, verbose_name='Стоимость услуги')
-    car = models.ForeignKey('Car', blank=True, null=True, on_delete=models.CASCADE, related_name='cars_ap', verbose_name='Машина')
-    user = models.ForeignKey('User', blank=True, null=True, on_delete=models.CASCADE, related_name='users_ap', verbose_name='Клиент')
+    car = models.ForeignKey('Car', blank=True, null=True, on_delete=models.CASCADE, related_name='cars_ap',
+                            verbose_name='Машина')
+    user = models.ForeignKey('User', blank=True, null=True, on_delete=models.CASCADE, related_name='users_ap',
+                             verbose_name='Клиент')
     service = models.ForeignKey('Service', on_delete=models.CASCADE, related_name='services_ap', verbose_name='Услуга')
     worker = models.ForeignKey('Worker', blank=True, null=True, on_delete=models.CASCADE, related_name='workers_ap',
                                verbose_name='Рабочий')
     email = models.EmailField(verbose_name='E-mail', blank=True, null=True, max_length=255, unique=True)
+    phone_number = models.CharField(max_length=12, unique=True,
+                                    validators=[phone_number_validator, MinLengthValidator(11)],
+                                    verbose_name='Номер телефона')
+    description = models.TextField(verbose_name='Описание проблемы', blank=True, null=True)
+    first_name = models.CharField(max_length=64, validators=[MinLengthValidator(2)], verbose_name='Имя')
+    last_name = models.CharField(max_length=128, validators=[MinLengthValidator(2)], verbose_name='Фамилие')
+
 
     def clean(self):
         if self.date < datetime.date.today():
@@ -107,14 +115,14 @@ class Reviews(models.Model):
 
 
 class Specialization(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
+    title = models.CharField(max_length=255, validators=[MinLengthValidator(4)], verbose_name='Название')
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
+    title = models.CharField(max_length=255, validators=[MinLengthValidator(4)], verbose_name='Название')
 
     def __str__(self):
         return self.title
