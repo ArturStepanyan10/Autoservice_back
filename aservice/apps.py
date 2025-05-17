ecf76db77988ecba4ae225d93c22f5ee63930f32
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-05-17 21:41:00 +0300

    feat(Notification): Signal connection

--- aservice/apps.py
+++ aservice/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AserviceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'aservice'
+
+    def ready(self):
+        import aservice.signals
